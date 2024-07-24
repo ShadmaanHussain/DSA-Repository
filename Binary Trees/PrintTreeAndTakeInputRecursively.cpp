c175afb49bf9f31e586786f32d9f696f2cdb8281
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-07-24 11:42:15 +0530

    Deleting all the exe files

--- Binary Trees/PrintTreeAndTakeInputRecursively.cpp
+++ Binary Trees/PrintTreeAndTakeInputRecursively.cpp
@@ -11,14 +11,38 @@ void PrintBinaryTree(BinaryTreeNode<int>* root) {
         cout << "L" << root->left->data << " ";
     }
     if(root->right) {
-        cout << "R" << root->right->data << endl;
+        cout << "R" << root->right->data;
     }
-
+    cout << endl;
     PrintBinaryTree(root->left);
     PrintBinaryTree(root->right);
 }
 
+BinaryTreeNode<int>* TakeInput() {
+    int rootData;
+    cout << "Enter Data: " << endl;
+    cin >> rootData;
+
+    // We are doing this because if we ask user several times if left exists or right exists it will be cumbersome for the user.
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* newNode = new BinaryTreeNode<int>(rootData);
+
+    newNode->left = TakeInput();
+    newNode->right = TakeInput();
+
+    return newNode;
+}
+
+    //      1
+    //     / \
+    //    2   3
+    // Input for the above tree
+    // 1 2 -1 -1 3 -1 -1
 int main() {
-    cout << "Hello World";
+    BinaryTreeNode<int>* root = TakeInput();
+    PrintBinaryTree(root);
+    delete root;
     return 0;
 }
