109cbbc44e4a3318010e4b02f982d62ac4eff5a5
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-07-28 11:38:59 +0530

    Count Nodes - Binary Tree

---
+++ Binary Trees/CountNodes.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include "BinaryTreeNode.h"
+using namespace std;
+
+int CountNodes(BinaryTreeNode<int>* root) {
+    if(root == NULL) return 0;
+
+    int leftCount = CountNodes(root->left);
+    int rightCount = CountNodes(root->right);
+
+    return 1 + leftCount + rightCount;
+}
+
+BinaryTreeNode<int>* TakeInputLevelWise() {
+    queue<BinaryTreeNode<int>*> nodes;
+    int nodeData;
+
+    cout << "Enter Node: " << endl;
+    cin >> nodeData;
+
+    if(nodeData == -1) return NULL;
+
+    BinaryTreeNode<int>* rootNode = new BinaryTreeNode<int>(nodeData);
+    nodes.push(rootNode);
+
+    while(!nodes.empty()) {
+        BinaryTreeNode<int>* current = nodes.front();
+        int leftChild, rightChild;
+        cout << "Enter the left Child of node " << current-> data << " :" << endl;
+        cin >> leftChild;
+        cout << "Enter the right Child of node " << current-> data << " :" << endl;
+        cin >> rightChild;
+
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftChildNode = new BinaryTreeNode<int>(leftChild);
+            current->left = leftChildNode;
+            nodes.push(leftChildNode);
+        }
+
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightChildNode = new BinaryTreeNode<int>(rightChild);
+            current->right = rightChildNode;
+            nodes.push(rightChildNode);
+        }
+
+        nodes.pop();
+    }
+
+    return rootNode;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = TakeInputLevelWise();
+    cout << "Nodes: " << CountNodes(root);
+    delete root;
+    return 0;
+}
